6b495ecdf1a8d17bc7ac5b0f8e433d657d1d8c2b
Madness98 <99425448+Madness98@users.noreply.github.com>
2022-03-24 22:04:33 +0100

    Checks for disabled computers

--- checks_for_disabled_computers.ps1
+++ checks_for_disabled_computers.ps1
@@ -1,7 +1,5 @@
 #This script checks and shows all disabled computers in the Active Directory
-
 Get-ADComputer -Filter {(Enabled -eq $False)} -ResultPageSize 2000 -ResultSetSize $null -Server <AnyDomainController> -Properties Name, OperatingSystem
 
 #If you want to export the result to a CSV file, use the Export-CSV cmdlet
-
 Get-ADComputer -Filter {(Enabled -eq $False)} -ResultPageSize 2000 -ResultSetSize $null -Server <AnyDomainController> -Properties Name, OperatingSystem | Export-CSV "C:\DisabledComputers.CSV" -NoTypeInformation
