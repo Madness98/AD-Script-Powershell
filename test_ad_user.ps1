3136f2c223f0cd85e0a4d90f1e6d35f3b85b6183
Madness98 <99425448+Madness98@users.noreply.github.com>
2022-03-24 22:13:16 +0100

    Update test_ad_user.ps1

--- test_ad_user.ps1
+++ test_ad_user.ps1
@@ -1,5 +1,8 @@
-$ADUserLogin = "NomUtilisateur"
-$ADUserPassword = "MotDePasse"
+#This script ensures that the username and password of an Active Directory account is correct by entering the username in the "$ADUserLogin" field and the password in the "$ADUserPassword" field
+
+
+$ADUserLogin = "Username"
+$ADUserPassword = "Password"
 
 Function Test-ADUserAuthentication {
     param(
